81f76a12fa860a1df4bcc6025f962b1eaffbb219
David Capella <D_Capella@yahoo.com>
2017-04-03 13:50:58 -0500

    end of assignment-5

--- models/address_book.rb
+++ models/address_book.rb
@@ -13,12 +13,20 @@ class AddressBook
     index = 0
     entries.each do |entry|
       # #10
-      if name < entry.name
-        break
-      end
+      break if name < entry.name
       index+= 1
     end
     # #11
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      break if (name.eql? entry.name) && (phone_number.eql? entry.phone_number) && (email.eql? entry.email)
+      index += 1
+    end
+    entries.delete_at(index)
+
   end
 end
